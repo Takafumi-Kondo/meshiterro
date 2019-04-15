e08d22aa3a3993d8f978d8156112b61a49f8b63f
kondo <aa@kondo.local>
2019-04-15 02:54:49 -0700

    [Finish] 5-2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+#devise機能使用前(ユーザ登録など)にconfigure_permitted_parametersが実行
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+#devise_parameter_sanitizer.permitでnameのデータ操作を許可するアクションメソッドが指定されている
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])#ユーザ登録、ユーザ名データ操作が許可
+	end
 end
