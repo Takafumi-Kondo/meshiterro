cff4cca95a09533a956a033691119fc614b41909
kondo <aa@kondo.local>
2019-04-15 20:45:17 -0700

    [Start] 5-5 投稿削除機能を作ろうまで

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -23,6 +23,12 @@ class PostImagesController < ApplicationController
 		@post_image = PostImage.find(params[:id])
 		@post_comment = PostComment.new
 	end
+#resourcesルーティングのdestroyからきたのを元に探しインスタンス変数へ
+	def destroy
+		@post_image = PostImage.find(params[:id])
+		@post_image.destroy
+		redirect_to post_images_path
+	end
 
 #投稿データのストロングパラメータ
 private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'post_images#index'
 
 #投稿コメは投稿画像の子要素になるので。
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	#単数だとそのコントローラのidがリクにない。コメント詳細は作らないのでidは不要
   	resource :post_comments, only: [:create, :destroy]
